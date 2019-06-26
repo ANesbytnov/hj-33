d9a7ab69ab6d285636650a625efe494633e79ad6
ANesbytnov <nesbytnovae@mail.ru>
2019-06-26 08:59:00 +0300

    HW 9. Release 2

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -2,7 +2,7 @@
 'use strict';
 
 function reCount(delta) {
-	localStorage.counter = delta === false || +localStorage.counter < -delta ? 0 : +localStorage.counter + delta;
+	localStorage.counter = delta === false || Number.isNaN(localStorage.counter) || +localStorage.counter < -delta ? 0 : +localStorage.counter + delta;
 	counter.textContent = localStorage.counter;
 }
 
